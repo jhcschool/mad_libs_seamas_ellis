d2d465802ca31c2098bb27897292b0ce6590593a
seamas.ohara99 <sohara@jhcschool.org>
2020-10-21 14:19:11 -0600

    New.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-#Seamas and Ellis worked together
-print("hello")
-print("hello")
-print("hello")
+# Seamas and Ellis worked together
+# Here is our madlib template:
 
+# Today I went to the zoo. I saw a(n) ___________(adjective) _____________(Noun) jumping up and down in its tree.
+# He _____________(verb, past tense) __________(adverb) through the large tunnel that led to its _______(adjective)
+# __________(noun). I got some peanuts and passed them through the cage to a gigantic gray _______(noun)
+# towering above my head. Feeding that animal made me hungry. I went to get a __________(adjective) scoop
+# of ice cream. It filled my stomach. Afterwards I had to __________(verb) __________ (adverb) to catch our bus.
+# When I got home I __________(verb, past tense) my mom for a __________(adjective) day at the zoo.
+
+is_running = True
+while is_running:
+    print("What's up? Today we will construct a functioning mad lib.")
+    print("When prompted, please enter a corresponding part of speech.")
+    user_command = input("")
